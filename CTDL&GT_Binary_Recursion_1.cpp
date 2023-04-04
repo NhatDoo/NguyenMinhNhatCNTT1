52b425a3721c3796e75e10b74882565b3713e796
NhatDoo <124895613+NhatDoo@users.noreply.github.com>
2023-04-04 15:41:10 +0700

    Create CTDL&GT_Binary_Recursion_1.cpp

---
+++ CTDL&GT_Binary_Recursion_1.cpp
@@ -0,0 +1,16 @@
+https://freetuts.net/de-quy-nhi-phan-binary-recursion-2953.html
+#include <iostream>
+using namespace std;
+ 
+int fib(int n){
+  if(n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+ 
+int main() {
+  int kq,n;
+  cout<<"Nhập vị trí n muốn tìm trong dãy fibonacci: ";
+  cin>>n;
+  kq = fib(n);
+  cout<<"Chương trình này được đăng tại Freetuts.net";
+}
